e1d414f2ca25ceeddf224e749a9ad9e5152cd0e9
Julia Alama <qa.julia.alama@gmail.com>
2023-05-28 20:43:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dicerollerapp/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.dicerollerapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Grey80 = Color(0xFF272727)
+val DarkGrey80 = Color(0xFF272727)
+val LightGrey80 = Color(0xFF2C2C2C)
+
+val LightGrey40 = Color(0xFF272727)
+val Grey40 = Color(0xFF272727)
+val LightGrey30 = Color(0xFF2C2C2C)
