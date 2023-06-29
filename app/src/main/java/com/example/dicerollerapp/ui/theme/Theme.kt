9c40fae48055c7752f78c0db21690fb7ca4ba8ec
Julia Alama <qa.julia.alama@gmail.com>
2023-06-29 21:47:25 +0200

    add affirmations, removes dices images

--- app/src/main/java/com/example/dicerollerapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/dicerollerapp/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun DiceRollerAppTheme(
+fun AffirnessApp(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+, set to false for training purposes.
     dynamicColor: Boolean = false,
